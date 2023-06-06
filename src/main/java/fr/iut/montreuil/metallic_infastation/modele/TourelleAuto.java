268e1956d85c2085d3fbf410a5719a1e976f614b
ThomasMarotta <thomasmarotta692@gmail.com>
2023-06-06 08:22:58 +0200

    Suppression fonctionnalite 'Tire sur 3 ennemis' Tourelle auto et ajout de la méthode 'infligerDegats' pour latourelle missile

--- src/main/java/fr/iut/montreuil/metallic_infastation/modele/TourelleAuto.java
+++ src/main/java/fr/iut/montreuil/metallic_infastation/modele/TourelleAuto.java
@@ -4,16 +4,15 @@ import java.util.ArrayList;
 
 public class TourelleAuto extends Tourelle{
     public TourelleAuto(Case position,Environnement env,Terrain terrain) {
-        super(5,position,30,5,env,terrain);
+        super(5,position,30,3,env,terrain, 0);
     }
+
 
     @Override
     public void infligerDegats() {
-        ArrayList<Ennemi> listeEnnemis = ennemisLesPlusProches(getPosition());
-        int tailleListe = Math.min(3, listeEnnemis.size());
-        //System.out.println(listeEnnemis.toString());
-        for (int i = 0 ; i < tailleListe; i++){
-            listeEnnemis.get(i).decrementerPv(getDegats());
+        if(getEnnemiVise() != null) {
+            getEnnemiVise().decrementerPv(getDegats());
+            System.out.println(getEnnemiVise().getPv());
         }
     }
 
--- src/main/java/fr/iut/montreuil/metallic_infastation/modele/TourelleSemi.java
+++ src/main/java/fr/iut/montreuil/metallic_infastation/modele/TourelleSemi.java
@@ -2,7 +2,7 @@ package fr.iut.montreuil.metallic_infastation.modele;
 
 public class TourelleSemi extends Tourelle {
     public TourelleSemi(Case position,Environnement env,Terrain terrain){
-        super(10,position,10,5,env,terrain);
+        super(10,position,10,5,env,terrain,0);
     }
 
     @Override
