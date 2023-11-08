b58b8fbc631146b75d5be35b720689f6b7497e45
TheKlaSik77 <kylian77280@gmail.com>
2023-11-08 15:48:22 +0100

    Modification Obstacle et ajout de l'interface effet pour eux

--- src/main/java/fr/iut/montreuil/metallic_infestation/modele/obstacles/Mine.java
+++ src/main/java/fr/iut/montreuil/metallic_infestation/modele/obstacles/Mine.java
@@ -1,42 +1,19 @@
 package fr.iut.montreuil.metallic_infestation.modele.obstacles;
 
-//import fr.iut.montreuil.metallic_infestation.modele.utilitaire.Explosion;
+import fr.iut.montreuil.metallic_infestation.modele.effets.Effet;
+import fr.iut.montreuil.metallic_infestation.modele.effets.Explosion;
+import fr.iut.montreuil.metallic_infestation.modele.ennemis.Ennemi;
 import fr.iut.montreuil.metallic_infestation.modele.utilitaire.Case;
 import fr.iut.montreuil.metallic_infestation.modele.utilitaire.Environnement;
 
 public class Mine extends Obstacle {
-
-    private int degats;
-
-
-    private int porteeExplosion;
     public Mine(Case c) {
-        super(c, 20);
-        this.degats = 300;
-        this.porteeExplosion = 2;
-    }
-
-    public int getDegats(){
-        return this.degats;
-    }
-    public int getPorteeExplosion() {
-        return porteeExplosion;
-    }
-    
-    @Override
-    public void actionObstacle() {
-        /*
-        Environnement.getInstance().getTerrain().setCase(this.getPosition(), 1);
-        Explosion explosion = new Explosion(this.getPosition().getCentreCase(), this.getDegats(), this.getPorteeExplosion());
-        Environnement.getInstance().getListExplosions().add(explosion);
-        explosion.infligerDegats();
-        Environnement.getInstance().getTourDeJeu().getObstaclesASupp().add(this);
-        */
-
+        super(c, 20, new Explosion(1,300,Environnement.getInstance()));
     }
 
     @Override
     public String getTypeObstacle() {
         return TypeObstacle.Mine.name();
     }
+
 }
