f208b5b90241a4aaf42db636e7f9c8b6ca07712c
MarielleVallee <marielle.vallee@gmail.com>
2023-11-15 15:44:02 +0100

    fonctionnel

--- src/main/java/fr/iut/montreuil/metallic_infestation/modele/tourEtProjectiles/ProjectileAuto.java
+++ src/main/java/fr/iut/montreuil/metallic_infestation/modele/tourEtProjectiles/ProjectileAuto.java
@@ -2,13 +2,11 @@ package fr.iut.montreuil.metallic_infestation.modele.tourEtProjectiles;
 
 
 import fr.iut.montreuil.metallic_infestation.modele.tourEtProjectiles.effets.DegatIndividuel;
-import fr.iut.montreuil.metallic_infestation.modele.tourEtProjectiles.effets.Effet;
 import fr.iut.montreuil.metallic_infestation.modele.utilitaire.Point;
 import fr.iut.montreuil.metallic_infestation.modele.ennemis.Ennemi;
 
-public class ProjectileAuto extends Projectile {
+public class ProjectileAuto extends Projectile{
     public ProjectileAuto(Point coordonnesDepart, Ennemi ennemiVise) {
-        super(new DegatIndividuel(10), coordonnesDepart, 1, ennemiVise);
+        super(new DegatIndividuel(10),coordonnesDepart,1,ennemiVise);
     }
 }
-
