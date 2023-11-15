b085d1cef73ccf8dd51eeaeaa0ab5cc282831ec9
TheKlaSik77 <kylian77280@gmail.com>
2023-11-15 16:15:04 +0100

    Remise à jour

--- src/main/java/fr/iut/montreuil/metallic_infestation/modele/effets/DegatIndividuel.java
+++ src/main/java/fr/iut/montreuil/metallic_infestation/modele/effets/DegatIndividuel.java
@@ -2,7 +2,7 @@ package fr.iut.montreuil.metallic_infestation.modele.effets;
 
 import fr.iut.montreuil.metallic_infestation.modele.ennemis.Ennemi;
 
-public class DegatIndividuel extends Degat{
+public class DegatIndividuel extends Degat {
 
     public DegatIndividuel(int degat){
         super(degat);
